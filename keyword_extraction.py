7b7ad5d940a06e693f223327c7c460ac867cf993
Harris Si <chensi2@illinois.edu>
2021-11-12 13:35:24 -0600

    add argparser

--- keyword_extraction.py
+++ keyword_extraction.py
@@ -3,11 +3,12 @@ import json
 import subprocess
 import pandas as pd
 from helpers import string_to_float
+import argparse
 
+# write this more like a module for more function usage
 
 
 def prepare_input_for_AutoPhrase(category):
-    print("Extracting category abstracts .........")
     arxiv_json = open("arxiv-metadata-oai-snapshot.json", 'r', encoding='utf-8')
     file = open("../AutoPhrase/data/EN/arxiv_abstract.txt", 'w', encoding='utf-8')
     for line in arxiv_json:
@@ -22,14 +23,12 @@ def prepare_input_for_AutoPhrase(category):
 
 
 def apply_auto_phrase():
-    print("Start AutoPhrase")
     cpp_command = "bash auto_phrase.sh"
     process = subprocess.run(cpp_command, shell=True, cwd='../AutoPhrase')
     print("AutoPhrase DONE")
 
 
 def extract_keywords_from_AutoPhrase():
-    print("Start extracting keywords from AutoPhrase")
     file = open("../AutoPhrase/models/DBLP/AutoPhrase.txt", 'r', encoding='utf-8')
     data = file.readlines()
     symbols = ["\\", "~", "$", "_", "-", "*", ":"]  # exclude terms including symbols
@@ -44,33 +43,40 @@ def extract_keywords_from_AutoPhrase():
             continue
         keywords_with_scores.append((line[13:-1], float(line[0:12])))
     file.close()
-    print("Extracting done")
+    print("Extracting from AutoPhrase done")
     # print(keywords_with_scores)
     return keywords_with_scores
 
+
+def simplify_result_keywords(keywords_with_scores, threshold):
+    keywords_with_scores.sort(reverse=True, key=lambda x: x[1])
+    revised_keywords = []
+    for pair in keywords_with_scores:
+        if pair[1] >= threshold:
+            revised_keywords.append(pair)
+    return revised_keywords
+
+
 def prepare_input_for_domain_relevance(keywords_with_scores):
-    print("Prepare Input for Domain-relevance")
     keywords = [pair[0] for pair in keywords_with_scores]
     f = open("../domain-relevance/input.txt", "w", encoding='utf-8')
     f.write(str(keywords))
     f.close()
-    print("Preparation done")
+    print("Preparation for Domain-relevance done")
 
 
 def apply_domain_relevant():
-    print("Start Getting Domain Relevance Score")
     command = "python3 query.py --domain cs --method cfl > output.txt"
     process = subprocess.run(command, shell=True, cwd='../domain-relevance')
     print("Domain Relevance Score DONE")
 
 
 def extract_keywords_from_domain_relevance():
-    print("Start extracting keywords from Domain-relevance")
     file = open("../domain-relevance/output.txt", "r", encoding="utf-8")
     domain_scores = file.readlines()[2:]
     keywords_with_scores = [line.split(': ') for line in domain_scores]
     keywords_with_scores = [(pair[0], string_to_float(pair[1].split('\n')[0])) for pair in keywords_with_scores]
-    print("Extracting done")
+    print("Extracting from Domain-Relevance done")
     return keywords_with_scores
 
 
@@ -86,23 +92,68 @@ def join_autophrase_domain_relevace_score(autophrase_scores, domain_relevance_sc
     keywords_with_scores.sort(key = lambda x: x[1], reverse=True)
     return keywords_with_scores
 
+def get_kewords_with_threshold(keywords_with_scores, threshold):
+    keywords = []
+    for pair in keywords_with_scores:
+        if pair[1] >= threshold:
+            keywords.append(pair)
+    return keywords
 
-def cleanup():
-    os.remove("../AutoPhrase/data/EN/arxiv_abstract.txt")
-    os.remove("../domain-relevance/input.txt")
-    os.remove("AutoPhrase_score.txt")
+
+def cleanup_autophrase():
+    file = "../AutoPhrase/data/EN/arxiv_abstract.txt"
+    if os.path.exists(file):
+        os.remove(file)
+
+
+def cleanup_domain_relevance():
+    file = "../domain-relevance/input.txt"
+    if os.path.exists(file):
+        os.remove(file)
+
+
+def cleanup_all():
+    cleanup_autophrase()
+    cleanup_domain_relevance()
+    
 
 def main():
-    # prepare_input_for_AutoPhrase("math")
-    # apply_auto_phrase()
-    autophrase_scores = extract_keywords_from_AutoPhrase()[:4000]
-    prepare_input_for_domain_relevance(autophrase_scores)
-    apply_domain_relevant()
+    parser = argparse.ArgumentParser(description='Domain-Relevant Keyword Extraction.')
+    parser.add_argument('-c', '--category', type=str, default='math', 
+                        help='ArXiv dataset category for base data, could be choose from "CS", "math", "Phy", default as "math"')
+    parser.add_argument('-t', '--threshold', type=float, default=0.88, 
+                        help='Threshold for finding final combined score that is higher than the threshold, should be number in (0, 1], default as 0.88')
+    parser.add_argument('-s', '--save-data-in-library', type=bool, default=False, 
+                        help="Whether to save data for both libraries, default as False")
+    parser.add_argument('-u', '--use-stored-data', type=bool, default=False, 
+                        help="Whether to use stored data in libraries, default as False")
+    # parser.add_argument('-w', "--combined_weight")
+
+    args = parser.parse_args()
+
+    if args.restart:
+        cleanup_all()
+    if args.category not in ["CS", "math", "Phy"]:
+        raise Exception("Category input does not match requirement, please select from \n CS \n Math \n Phy")
+    if args.threshold <= 0 or args.threshold > 1:
+        raise Exception("Threshold input does not match requirement, please select number in (0, 1]")
+
+    if not args.use_stored_data:
+        prepare_input_for_AutoPhrase(args.category)
+    if not args.use_stored_data:
+        apply_auto_phrase()
+    autophrase_scores = extract_keywords_from_AutoPhrase()
+    autophrase_scores = simplify_result_keywords(autophrase_scores, 0.85)
+
+    if not args.use_stored_data:
+        prepare_input_for_domain_relevance(autophrase_scores)
+        apply_domain_relevant()
     domain_relevance_scores = extract_keywords_from_domain_relevance()
-    # print(domain_relevance_scores)
+
     combined_scores = join_autophrase_domain_relevace_score(autophrase_scores, domain_relevance_scores, 1, 15)
-    print(combined_scores[:40])
-    # cleanup()
+    print(get_kewords_with_threshold(combined_scores, args.threshold))
+    if not args.save_data_in_library:
+        cleanup_all()
 
 
 if __name__ == '__main__':
